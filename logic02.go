e2349d7615d328ac381596fed5630808cdd58f74
renaldid <purwantorenaldi@gmail.com>
2022-05-30 21:57:53 +0700

    penambahan soal05dan06 pada logic02

--- logic02.go
+++ logic02.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	Logic02Soal03(9)
+	Logic02Soal06(9)
 }
 func Logic02Soal01(n int) {
 	a := 3
@@ -64,5 +64,52 @@ func Logic02Soal04(n int) {
 		//pindah ke baris selanjutnya
 		fmt.Println("\n")
 		a -= 3
+	}
+}
+
+func Logic02Soal05(n int) {
+	a := 3
+	//ini nilai tengah
+	tengah := n / 2
+
+	//looping baris
+	for i := 0; i < n; i++ {
+		//loopimg kolom
+		for j := 0; j < n; j++ {
+			fmt.Print(a, "\t")
+		}
+		//kolom selesai
+		//kemudian pindah baris selanjutnya
+		fmt.Println("\n")
+		if i < tengah {
+			a += 3
+			fmt.Print(a, "\t")
+		} else {
+			a -= 3
+			fmt.Print(a, "\t")
+		}
+	}
+}
+
+func Logic02Soal06(n int) {
+	tengah := n / 2
+
+	//looping baris
+	for i := 0; i < n; i++ {
+		a := 3
+
+		//looping kolom
+		for j := 0; j < n; j++ {
+			if j < tengah {
+				fmt.Print(a, "\t")
+				a += 3
+			} else {
+				fmt.Print(a, "\t")
+				a -= 3
+			}
+
+		}
+		//ini untuk pindah baris
+		fmt.Println("\n")
 	}
 }
